fffb98f61ed29234b52356aa9f4aa3dbc8a1c4e3
Angie Gonzalez <angiem.go@gmail.com>
2022-11-28 18:49:46 -0600

    getting setup

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
+    <h1>Vocab-YOU-lary!</h1>
     <small>Open your dev tools</small><br />
     <button class="btn btn-danger" id="click-me">Click ME!</button><br />
     <hr />
